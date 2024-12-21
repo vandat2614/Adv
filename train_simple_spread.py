41e9a18772178b5eea4472674bb1502e3fcdc7d4
Van Dat <dovandat2614@gmail.com>
2024-12-21 18:11:04 +0700

    Update train_simple_spread.py

--- train_simple_spread.py
+++ train_simple_spread.py
@@ -41,7 +41,6 @@ for i in range(num_episodes):
             matd3_agent.update_critic(buffer)
             count += 1
         if (count+1)%update_rate==0 and buffer.ready():
-            print(f'UPDATE ACTOR')
             matd3_agent.update_actor(buffer)
             matd3_agent.update_target_actor()
             matd3_agent.update_target_critic()
